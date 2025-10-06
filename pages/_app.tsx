3eece97fdeeff7dc87147a41900e754629d784c4
junpengpanchina-ai <junpengpanchina@gmail.com>
2025-10-06 12:57:52 +0800

    Fix CSS import issue by using styled-jsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <style jsx global>{`
+        @tailwind base;
+        @tailwind components;
+        @tailwind utilities;
+      `}</style>
       <Component {...pageProps} />
       <SpeedInsights />
     </>
