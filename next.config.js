065aa41c349905e6d98648cc659c8c36cf3fdfed
junpengpanchina-ai <junpengpanchina@gmail.com>
2025-10-11 00:11:08 +0800

    1

--- next.config.js
+++ next.config.js
@@ -4,12 +4,16 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ['localhost'],
   },
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
+  outputFileTracingRoot: __dirname,
 }
 
 module.exports = nextConfig
